2213f47c5613a0722f8668fd4d5080544e04a3e1
Monika Allen <mokallen17@gmail.com>
2023-02-07 11:42:49 -1000

    Lab 4

---
+++ Lab5/exercise1.js
@@ -0,0 +1,11 @@
+age =  20;
+fav_num = 2;
+day_of_birth = 17;
+month_of_birth = 1;
+
+console.log(
+age + fav_num / day_of_birth * month_of_birth
+);
+console.log(
+    (age + fav_num) / day_of_birth * month_of_birth
+);
