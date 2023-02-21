259a7d6ccd4573c4178522467bd99dc8d0dc6465
Monika Allen <mokallen17@gmail.com>
2023-02-21 12:55:16 -1000

    Lab 7

---
+++ Lab7/Ex2a.js
@@ -0,0 +1,12 @@
+require("./products_data.js");
+
+var num_products = 5;
+var prod_number = 1;
+while(prod_number <= num_products) {
+    console.log(`${prod_number}. ${eval('name' + prod_number)}`);
+    prod_number++;
+    if (prod_number > num_products/2) {
+        break;
+    }
+}
+console.log("That's enough!");
