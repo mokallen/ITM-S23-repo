41d004aafa21c372966c1d6ea84c12debc8089ae
Monika Allen <mokallen17@gmail.com>
2023-04-10 23:21:14 -1000

    assignment 1

--- Allen_Monika_Assignment1/server.js
+++ Allen_Monika_Assignment1/server.js
@@ -1,8 +1,7 @@
+// server.js runs the server for store page requests
 var express = require('express'); // requiring express and not http, which will contain http and more
 var app = express(); // creating object app to express
-
-var products = require(__dirname + '/products.json');
-console.log(products);
+var qs = require('querystring');
 
 app.use(express.urlencoded({ extended: true })); // wil automatically decode any data submitted, put keys/values in the data to put in the body
 
@@ -10,36 +9,71 @@ app.use(express.urlencoded({ extended: true })); // wil automatically decode any
 
 // monitor all requests
 app.all('*', function (request, response, next) { // any request for any path, executes function (* means any)
-   console.log(request.method + ' to path ' + request.path + ' with qs ' + JSON.stringify(request.query));
+   console.log(request.method + ' to path ' + request.path);
    next();
 });
 
-// process purchase request (validate quantities, check quantity available)
-// trying to fix cannot get error
-app.get('/', function(req, res) {
-   res.send('Hello, world!');
- });
- 
+// create variable products from json file and store it on the server
+var products = require(__dirname + '/products.json');
+   console.log(products); // to check that products are being loaded on the server
+
+// track quantity sold
+products.forEach( (prod,i) => {prod.total_sold = 0});
+
+// process purchase request (validate quantities, check quantity available) monitor requests
 app.get("/product_data.js", function (request, response, next) {
    response.type('.js');
    var products_str = `var products = ${JSON.stringify(products)};`;
    response.send(products_str);
-}); // microservice, when someone asks for this, it is not being take from a file. it is giving back javascript. asking for data, sever gives data. there are better ways to do this, asking server for json data
+}); // sends string of data when requested
 
-app.post('/process_purchase', function (request, response, next) { //route that is supposed to recieve the request when user submits #'s of products
-   console.log(request.body);
-   // validate quantities, all validations must be done on the server
-   // gonna need a loop for all the quanities in the body
-   // at least 1 selection, use a flag variable (false or true boolean, start above the loop has selections false > is q > 0? has quantities becomes false to true)
-   var q = request.body['quantity_textbox'];
-   if (typeof q != 'undefined') {
-       response.send(`Thank you for purchasing ${q} things!`);
-   } 
+// **IR1 Task: IR1 Track the total quantity of each item sold. This needs to be implemented on the server when you remove sold items from the quantity available. Display total quantity sold with the product information.
+app.post('/process_form', function (request, response) { //route that is supposed to recieve the request when user submits #'s of products
+   var POST = request.body;
+   // *note to self: validate quantities, all validations must be done on the server
+   // *note to self: gonna need a loop for all the quanities in the body
+   // *note to self: at least 1 selection, use a flag variable (false or true boolean, start above the loop has selections false > is q > 0? has quantities becomes false to true)
+   // assume no quantities
+   var Valid_Purchase = false;
+   // errorObject to store error messages
+   var errorsObject = {};
 
-// if valid, complete the purchase
+   // for loop to check if there is a valid quantity input
+   for (let i = 0; i < products.length; i++) {
+      qtys = POST[`quantity`+i]; // retrieves quantity input from POST method
 
-// if not valid, send errors and go back to porder page
-//remove from inventory, loop again through quantities and deduct from the products array quantities available for that product. update from memory? serve products from the server.
+   // referenced from Assignment 1 workshop: checks if it is a valid request
+   // if there are no quantities selected, this will push an error and create a flag to see if there are quantities in the input boxes
+   Valid_Purchase = Valid_Purchase || (qtys > 0);
+
+   // isNonNegInt function referenced from Lab9? **need to double check**
+   if (isNonNegInt(qtys, false) == false) {
+      errorsObject[`q_error${i}`] = isNonNegInt(qtys, true); // if there is an error, it will take the values of the error and put it into this custom error message
+   }
+}
+
+   //*note to self: remove from inventory, loop again through quantities and deduct from the products array quantities available for that product. update from memory? serve products from the server.
+   // if there are no empty boxes and purchase is valid, it will take the quantity inputted by user as the number of products sold
+   if ((Valid_Purchase == true) && (Object.keys(errorsObject).length == 0)) {
+      // calculates quantity sold by adding whatever the user has input
+      for (let i in products) {
+         qtys = POST[`quantity`+i];
+         products[i].total_sold += Number(qtys); // add user input quantities
+         products[i].quantity_availabe = products[i].quantity_available - Number(qtys); // quantity available minus input quantities to calculate how many products are left for sale
+      }
+      response.redirect("./invoice.html?" + qs.stringify(request.body)); // if valid, complete the purchase -> redirect to the inovice page, placed outside of the loop to ensure only one response is submitted
+   }
+   // if not valid, send errors and go back to order page
+   // if all quantity boxes are empty and purchase is invalid, redirect to products_display page
+   else if ((Valid_Purchase == false) && (Object.keys(errors.Object).length == 0)) {
+      response.redirect("./products_display.html?" + qs.stringify(request.body) + `&noQuantities=Please enter a quantity`); // displays message with appended key: noQuantities and value: "Please enter a quantity"
+   }
+   // if there is an input error, redirect to products_display with appended key: inputError and value: "Please correct all errors"
+   // append the errorsObject as a string
+   else if ((Object.keys(errorsObject).length > 0)) {
+      response.redirect("./products_display.html?" + qs.stringify(request.body) + `&inputError=Please correct all errors` + `&` + qs.stringify(errorsObject));
+      console.log(errorsObject);
+   }
 });
 
 // route all other GET requests to files in public 
